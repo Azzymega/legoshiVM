51f36e6092b0a5aa9598c39b87e581fdc3f2caad
RedDawn <jdjdjd33loxru@gmail.com>
2023-08-30 22:37:46 +0300

    start working on javac

---
+++ src/javac/main.cpp
@@ -0,0 +1,11 @@
+/*
+ * PROJECT:     LegoshiVM - Java language compiler
+ * LICENSE:     GPL - See COPYING in the top level directory
+ * PROGRAMMER:  Maltsev Daniil <brickexberiment@lenta.ru>
+*/
+
+#include "jlexer.h"
+
+int main(){
+    return yylex();
+}
